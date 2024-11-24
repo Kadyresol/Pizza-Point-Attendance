ff98759902bb0afa97179e52d2a4eb48edd9efc8
Kadyresol <kadyresol@gmail.com>
2024-11-24 12:49:47 -0800

    qwe

--- pizzapointattendance/manage.py
+++ pizzapointattendance/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks."""d
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzapointattendance.settings')
     try:
         from django.core.management import execute_from_command_line
