8d450dfad3dcedd370c79f6405fe16d6e3d0142a
Kadyresol <kadyresol@gmail.com>
2024-11-27 12:10:52 -0800

    committed

--- app/serializers.py
+++ app/serializers.py
@@ -15,11 +15,11 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
 class EmployeeDetailSerializer(serializers.ModelSerializer):
 
-    #records = RecordSerializer(many=True, read_only=True)
+    records = RecordSerializer(many=True, read_only=True)
 
     class Meta:
         model = Employee
-        fields = ['id', 'name', 'surname', 'phone_number', 'hourly_earning', 'pay_price', 'status']
+        fields = ['id', 'name', 'surname', 'phone_number', 'hourly_earning', 'pay_price', 'status', 'records']
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
